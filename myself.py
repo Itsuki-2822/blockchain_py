1ffc36dd7b3539a02074d10238f2394d38b362d7
Itsuki <itoitsuki.28@gmail.com>
2023-06-24 13:22:21 +0900

    初めてのコミット

---
+++ myself.py
@@ -0,0 +1,96 @@
+### Date ###
+#6月8日 18:30
+
+### TODO ###
+#マイニング難易度の調整: 現在のコードでは、マイニングの難易度を指定していますが、動的に調整するメカニズムを導入することで、ネットワークのハッシュパワーに基づいて難易度を自動的に調整できます。
+#トランザクションの追加: 現在のコードでは、ブロックにデータを含めることができますが、より実用的なブロックチェーンを構築するためには、トランザクションの追加やトランザクションの検証機能を実装することが重要です。
+#ブロックの検証: ブロックチェーンにおいては、ブロックが正当であるかどうかを検証する仕組みが必要です。ブロックのハッシュや直前のブロックのハッシュなど、様々な要素を検証するロジックを導入することで、ブロックチェーンのセキュリティを強化できます。
+#ブロックチェーンの永続化: 現在のコードでは、ブロックチェーンはメモリ内に保持されます。しかし、実際のブロックチェーンでは、ブロックチェーンのデータをディスク上に永続化する必要があります。データベースやファイルシステムを使用してブロックチェーンのデータを保存する機能を追加することが望ましいです。
+
+
+from datetime import datetime, timezone, timedelta
+import hashlib
+import json
+import wallet
+
+jst = timezone(timedelta(hours=+9), 'JST')
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.wallets = {}
+
+    def genesis_block(self, index, data, nonce, previous_hash, Block_hash):
+        JAPAN_time = datetime.now(jst).strftime("%Y-%m-%d %H:%M:%S")
+        genesis_data = {
+            'index': index,
+            'timestamp': JAPAN_time,
+            'data': data,
+            'nonce': nonce,
+            'previous_hash': previous_hash,
+            'Block_hash': Block_hash,
+            'transactions': []
+        }
+        self.chain.append(genesis_data)
+        return genesis_data
+
+    def calculate_hash(self, block):
+        block_str = json.dumps(block, sort_keys=True)
+        block_hash = hashlib.sha256(block_str.encode()).hexdigest()
+        return block_hash
+
+    def mine_block(self, data,Block_hash):
+        JAPAN_time = datetime.now(jst).strftime("%Y-%m-%d %H:%M:%S")
+        previous_block = self.chain[-1]
+        previous_hash = previous_block['Block_hash']
+        index = previous_block['index'] + 1
+        nonce = 0
+
+        mining_difficulty = '00000'
+        while True:
+            block = {
+                'index': index,
+                'timestamp': JAPAN_time,
+                'data':data,
+                'nonce': nonce,
+                'previous_hash': previous_hash,
+                'Block_hash': None,
+            }
+            self.block_hash = self.calculate_hash(block)
+
+            if self.block_hash[:len(mining_difficulty)] == mining_difficulty:
+                block['Block_hash'] = self.block_hash
+                self.chain.append(block)
+                break
+
+            nonce += 1
+
+        return self.chain[-1]
+
+
+def print_chain(chain):
+    print("------------------------------------")
+    for block in chain:
+        print(" ")
+        print("index:", block['index'])
+        print("timestamp:", block['timestamp'])
+        print("data:", block['data'])
+        print("nonce:", block['nonce'])
+        print("previous_hash:", block['previous_hash'])
+        print("Block_hash:", block['Block_hash'])
+        print(" ")
+    print("------------------------------------")
+
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+
+    genesis_block = blockchain.genesis_block(1, "GENESIS", None, None, blockchain.calculate_hash({}))
+    print_chain(blockchain.chain)
+    print(" ")
+
+    # ブロックのマイニングとチェーンの更新
+    block1 = blockchain.mine_block("いつき",blockchain.chain[-1]['Block_hash'])
+    print_chain(blockchain.chain)
+    print(" ")
---
+++ wallet.py
@@ -0,0 +1,54 @@
+import base58
+import codecs
+import hashlib
+
+from ecdsa import NIST256p
+from ecdsa import SigningKey
+
+
+class Wallet(object):
+
+    def __init__(self):
+        self._private_key = SigningKey.generate(curve=NIST256p)
+        self._public_key = self._private_key.get_verifying_key()
+        self._blockchain_address = self.generate_blockchain_address()
+
+    @property
+    def private_key(self):
+        return self._private_key.to_string().hex()
+
+    @property
+    def public_key(self):
+        return self._public_key.to_string().hex()
+
+    @property
+    def blockchain_address(self):
+        return self._blockchain_address
+
+    def generate_blockchain_address(self):
+        # 公開鍵をバイト列に変換し、SHA-256ハッシュを計算する
+        public_key_bytes = self._public_key.to_string()
+        sha256_bpk_digest = hashlib.sha256(public_key_bytes).digest()
+
+        # RIPEMD-160ハッシュを計算する
+        ripemed160_bpk_digest = hashlib.new('ripemd160', sha256_bpk_digest).digest()
+
+        # ネットワークバイトを追加してアドレスを形成する
+        network_bitcoin_public_key = b'00' + ripemed160_bpk_digest
+
+        # ハッシュのチェックサムを計算する
+        sha256_2_nbpk_digest = hashlib.sha256(hashlib.sha256(network_bitcoin_public_key).digest()).digest()
+        checksum = sha256_2_nbpk_digest[:8]
+
+        # アドレスの16進数表現を生成する
+        address_hex = (network_bitcoin_public_key + checksum).hex()
+
+        # Base58エンコードしてブロックチェーンアドレスを生成する
+        blockchain_address = base58.b58encode(bytes.fromhex(address_hex)).decode('utf-8')
+        return blockchain_address
+
+if __name__ == '__main__':
+    wallet = Wallet()
+    print("PRIVATE_key :",wallet.private_key)
+    print("PUBLIC_key :",wallet.public_key)
+    print("ADDRESS :",wallet.blockchain_address)
