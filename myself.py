f0c7ea487e2273e772e2093c208bce8381da3396
Itsuki <itoitsuki.28@gmail.com>
2023-07-03 13:20:01 +0900

    walletファイルへの分割と認証の正規動作

--- myself.py
+++ myself.py
@@ -16,6 +16,8 @@ import datetime
 from datetime import timezone
 from ecdsa import SigningKey, NIST256p
 import base58
+import wallet
+
 
 class Blockchain(object):
     def __init__(self):
@@ -67,57 +69,7 @@ class Blockchain(object):
 
         return block
 
-class Wallet(object):
-    def __init__(self, recipient_address=None, value=None):
-        self._private_key = SigningKey.generate(curve=NIST256p)
-        self._public_key = self._private_key.get_verifying_key()
-        self._blockchain_address = self.generate_blockchain_address()
-        self.recipient_address = recipient_address
-        self.value = value
 
-    @property
-    def private_key(self):
-        return self._private_key.to_string().hex()
-
-    @property
-    def public_key(self):
-        return self._public_key.to_string().hex()
-
-    @property
-    def blockchain_address(self):
-        return self._blockchain_address
-
-    def generate_blockchain_address(self):
-        public_key_bytes = self._public_key.to_string()
-        sha256_bpk_digest = hashlib.sha256(public_key_bytes).digest()
-        ripemed160_bpk_digest = hashlib.new('ripemd160', sha256_bpk_digest).digest()
-        network_bitcoin_public_key = b'00' + ripemed160_bpk_digest
-        sha256_2_nbpk_digest = hashlib.sha256(hashlib.sha256(network_bitcoin_public_key).digest()).digest()
-        checksum = sha256_2_nbpk_digest[:8]
-        address_hex = (network_bitcoin_public_key + checksum).hex()
-        blockchain_address = base58.b58encode(bytes.fromhex(address_hex)).decode('utf-8')
-        return blockchain_address
-
-    def create_transaction(self):
-        signature = self.generate_signature()
-        transaction = {
-            'sender_blockchain_address': self.blockchain_address,
-            'recipient_blockchain_address': self.recipient_address,
-            'value': self.value,
-            'signature': signature
-        }
-        return transaction
-
-    def generate_signature(self):
-        transaction_data = {
-            'sender_blockchain_address': self.blockchain_address,
-            'recipient_blockchain_address': self.recipient_address,
-            'value': float(self.value)
-        }
-        transaction_str = json.dumps(transaction_data, sort_keys=True).encode()
-        private_key = SigningKey.from_string(bytes.fromhex(self.private_key), curve=NIST256p)
-        signature = private_key.sign(transaction_str)
-        return signature.hex()
     
 def get_transaction_details():
     recipient_address = input("Enter recipient address: ")
@@ -131,7 +83,7 @@ if __name__ == "__main__":
     recipient_address = input("Enter recipient address: ")
     value = float(input("Enter value: "))
 
-    wallet = Wallet(recipient_address=recipient_address, value=value)
+    wallet = wallet.Wallet(recipient_address=recipient_address, value=value)
     print("Private Key:", wallet.private_key)
     print("Public Key:", wallet.public_key)
     print("Blockchain Address:", wallet.blockchain_address)
--- wallet.py
+++ wallet.py
@@ -2,19 +2,21 @@
 # 認証の機能追加 6/27
 # 認証の機能の詳しい理解と応用
 
-import base58
-import codecs
+import json
 import hashlib
-from ecdsa import NIST256p
-from ecdsa import SigningKey
+import datetime
+from datetime import timezone
+from ecdsa import SigningKey, NIST256p
+import base58
 
 
 class Wallet(object):
-
-    def __init__(self):
+    def __init__(self, recipient_address=None, value=None):
         self._private_key = SigningKey.generate(curve=NIST256p)
         self._public_key = self._private_key.get_verifying_key()
         self._blockchain_address = self.generate_blockchain_address()
+        self.recipient_address = recipient_address
+        self.value = value
 
     @property
     def private_key(self):
@@ -29,47 +31,36 @@ class Wallet(object):
         return self._blockchain_address
 
     def generate_blockchain_address(self):
-        # 公開鍵をバイト列に変換し、SHA-256ハッシュを計算する
         public_key_bytes = self._public_key.to_string()
         sha256_bpk_digest = hashlib.sha256(public_key_bytes).digest()
-
-        # RIPEMD-160ハッシュを計算する
         ripemed160_bpk_digest = hashlib.new('ripemd160', sha256_bpk_digest).digest()
-
-        # ネットワークバイトを追加してアドレスを形成する
         network_bitcoin_public_key = b'00' + ripemed160_bpk_digest
-
-        # ハッシュのチェックサムを計算する
         sha256_2_nbpk_digest = hashlib.sha256(hashlib.sha256(network_bitcoin_public_key).digest()).digest()
         checksum = sha256_2_nbpk_digest[:8]
-
-        # アドレスの16進数表現を生成する
         address_hex = (network_bitcoin_public_key + checksum).hex()
-
-        # Base58エンコードしてブロックチェーンアドレスを生成する
         blockchain_address = base58.b58encode(bytes.fromhex(address_hex)).decode('utf-8')
         return blockchain_address
 
-class Transaction(object):
-    def __init__(self,sender_private_key,sender_public_key,
-                 sender_blockchain_address,recipient_blockchain_address,value):
-        self.sender_private_key = sender_private_key
-        self.sender_public_key = sender_public_key
-        self.sender_blockchain_address = sender_blockchain_address
-        self.recipient_blockchain_address = recipient_blockchain_address
-        self.value = value
+    def create_transaction(self):
+        signature = self.generate_signature()
+        transaction = {
+            'sender_blockchain_address': self.blockchain_address,
+            'recipient_blockchain_address': self.recipient_address,
+            'value': self.value,
+            'signature': signature
+        }
+        return transaction
 
     def generate_signature(self):
-        transacrion = {
-            'sender_blockchain_address':self.sender_blockchain_address,
-            'recipient_blockchain_address':self.recipient_blockchain_address,
-            'value':float(self.value)
+        transaction_data = {
+            'sender_blockchain_address': self.blockchain_address,
+            'recipient_blockchain_address': self.recipient_address,
+            'value': self.value
         }
-        message = hashlib.sha256().digest()
-        private_key = SigningKey.from_string(bytes().fromhex(self.sender_private_key),curve = NIST256p)
-        private_key_sign = private_key.sign(message)
-        signature = private_key_sign.hex()
-        return signature
+        transaction_str = json.dumps(transaction_data, sort_keys=True).encode()
+        private_key = SigningKey.from_string(bytes.fromhex(self.private_key), curve=NIST256p)
+        signature = private_key.sign(transaction_str)
+        return signature.hex()
 
 
 if __name__ == '__main__':
@@ -77,6 +68,4 @@ if __name__ == '__main__':
     print("private_key :",wallet.private_key)
     print("public_key :",wallet.public_key)
     print("address :",wallet.blockchain_address)
-    
-    transaction = Transaction(wallet.private_key,wallet.public_key,wallet.blockchain_address,"B",1.0)
-    print("signature :",transaction.generate_signature())
+    print("signature :",wallet.generate_signature())
